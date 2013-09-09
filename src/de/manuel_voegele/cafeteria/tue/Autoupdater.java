608741c1c7e153db066dff2846996d8b7706e956
Manuel Vögele <develop@manuel-voegele.de>
2013-09-09 23:26:37 +0200

    Javadoc and styling

--- src/de/manuel_voegele/cafeteria/tue/Autoupdater.java
+++ src/de/manuel_voegele/cafeteria/tue/Autoupdater.java
@@ -10,6 +10,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
+/**
+ * This class is responsible for updating the menu every day at 4 am.
+ * 
+ * @author Manuel Vögele
+ * 
+ */
 public class Autoupdater extends BroadcastReceiver {
 
 	/** {@link Intent} to update the menu automatically */
@@ -35,8 +41,10 @@ public class Autoupdater extends BroadcastReceiver {
 	 * 
 	 * @param cafeteriaId
 	 *           the id of the cafeteria to refresh the menus for
+	 * @param context
+	 *           the context
 	 */
-	public void refreshMenus(Context context,int cafeteriaId)
+	public static void refreshMenus(Context context, int cafeteriaId)
 	{
 		Intent intent = new Intent(context, UpdateMenusService.class);
 		intent.putExtra("cafeteriaid", cafeteriaId);
@@ -44,28 +52,27 @@ public class Autoupdater extends BroadcastReceiver {
 	}
 	
 	/**
-	 * Set an Alarm for the next Update <br>
-	 * This funktion is threadsave (synchronized)
+	 * Set an alarm for the next update. This function is thread save
+	 * (synchronized)
 	 * 
 	 * @param context
-	 *            The {@link Context} of the current application
+	 *           the context
 	 */
-	synchronized public static void nextUpdate(Context context) {
+	public static synchronized void nextUpdate(Context context)
+	{
 		// The Intent, witch send the signal for the update
 		Intent i_alarm = new Intent();
 		
 		//Set the time for the next update
 		Calendar updateTime = Calendar.getInstance();
-		updateTime.add(Calendar.HOUR_OF_DAY, 20); //Update at 4 o'clock. If the current time is before 4 o'clock the alarm will set today, else it will be called tomorrow
+		updateTime.add(Calendar.HOUR_OF_DAY, 20); //Update at 4 am. If the current time is before 4 o'clock the alarm will set today, else it will be called tomorrow
 		updateTime.set(Calendar.HOUR_OF_DAY, 4);
 		updateTime.set(Calendar.MINUTE, 0);
 		updateTime.set(Calendar.SECOND, 0);
 		updateTime.set(Calendar.MILLISECOND, 0);
 		
 		//Create the update period intent
-		i_alarm.setComponent(new ComponentName(
-				"de.manuel_voegele.cafeteria.tue", Autoupdater.class
-						.getName()));
+		i_alarm.setComponent(new ComponentName("de.manuel_voegele.cafeteria.tue", Autoupdater.class.getName()));
 		i_alarm.setAction(ACTION_UPDATE);
 
 		//Set the AlarmManager to call the Intent at the specified time
